c9b4ea93c80046db1ad52db7e6097a7cdc75f3c5
fuwangqin <382018309@qq.com>
2017-04-06 12:01:37 +0800

    add codes

--- src/actor/message.hpp
+++ src/actor/message.hpp
@@ -56,31 +56,6 @@ namespace romi
 
 	template<typename T>
 	message_base::ptr make_message(const addr &, const addr &, const T &);
-
-	class message_builder
-	{
-	public:
-		using build_handle =
-			std::function<message_base::ptr(const void*, std::size_t)>;
-		static message_builder &instance()
-		{
-			static message_builder inst;
-			return inst;
-		}
-		void regist(std::string message_type, const build_handle &handle)
-		{
-			std::lock_guard<std::mutex> locker(locker_);
-			handles_[message_type] = handle;
-		}
-		build_handle get_message_build_handle(const std::string &message_type)
-		{
-			std::lock_guard<std::mutex> locker(locker_);
-			return handles_[message_type];
-		}
-	private:
-		std::mutex locker_;
-		std::map<std::string, build_handle> handles_;
-	};
 }
 
 
--- src/actor/message_builder.hpp
+++ src/actor/message_builder.hpp
@@ -1,5 +1,28 @@
 #pragma once
 namespace romi
 {
-	
+	class message_builder
+	{
+	public:
+		using build_handle =
+			std::function<message_base::ptr(const void*, std::size_t)>;
+		static message_builder &instance()
+		{
+			static message_builder inst;
+			return inst;
+		}
+		void regist(std::string message_type, const build_handle &handle)
+		{
+			std::lock_guard<std::mutex> locker(locker_);
+			handles_[message_type] = handle;
+		}
+		build_handle get_message_build_handle(const std::string &message_type)
+		{
+			std::lock_guard<std::mutex> locker(locker_);
+			return handles_[message_type];
+		}
+	private:
+		std::mutex locker_;
+		std::map<std::string, build_handle> handles_;
+	};
 }
